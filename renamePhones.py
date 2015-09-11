8123cba2fb5e7c51172c8b4717c6fa1fc460876c
mjcastro1984 <miguel.castro@cdw.com>
2015-09-11 11:07:45 -0400

    commit renamePhones.py to master

--- renamePhones.py
+++ renamePhones.py
@@ -1,4 +1,4 @@
-# rename BAT export to HNB naming conventions
+# rename BAT Phones export to new naming conventions
 # 04-SEPT-2015
 # Author: Miguel Castro
 # Collaborator: Patrick Nowicki
@@ -7,6 +7,12 @@ import csv
 import sys
 import re
 
+print "Renaming objects in BAT Phones export"
+
+## the following functions complete individual rename tasks for the specific
+## object referenced
+
+# rename Calling Search Spaces
 def renameCSS ( css ):
 	if css == '':
 		css = ''
@@ -18,6 +24,7 @@ def renameCSS ( css ):
 		css = css[:50]
 		return css
 
+# rename Partitions
 def renamePT ( pt ):
 	if pt == "Cluster DN Presence Allowed":
 		pt = "STAGING_DN_PT"
@@ -25,40 +32,54 @@ def renamePT ( pt ):
 		pt = "STAGING_DN_PT"
 	return pt
 
+# rename Device Pools
 def renameDevPool ( devpool ):
 	devpool = devpool.upper()
 	devpool = devpool.replace (" ", "_")
 	devpool = devpool + "_DP"
 	return devpool
 
+# rename Locations
 def renameLocation ( location ):
 	location = location.upper()
 	location = location.replace (" ", "_")
 	location = location + "_LOC"
 	return location
 
+# rename MRGLs
 def renameMRGL ( mrgl ):
 	mrgl = mrgl.upper()
 	mrgl = mrgl.replace (" ", "_")
 	return mrgl
-	
+
+# rename Voice Mail Profiles
 def renameVMP ( vmp ):
 	if vmp == "ExchangeUM":
 		vmp = "HNB-Standard-VM"
 	return vmp
-	
+
+# modify BLF Directory Number field Partition references
 def renameBLF ( blf ):
 	blf = re.sub(r"(.*) Cluster DN Presence (.*)", r'\1 STAGING_DN_PT', blf)
 	return blf
 
+# open file based on parameters passed on script execution
+print "Opening input file"
 f = open(sys.argv[1], 'rb') # open input CSV file
 try:
 	reader = csv.reader(f)	# creates reader object
 	rownum = 0
-	outputfile = open ('phone-updated.csv', 'wb')
+	outputfilename = 'phone-updated.csv'
+	outputfile = open ( outputfilename, 'wb')	# creates writer object
+	print "Output file is %s" % outputfilename
 	a = csv.writer(outputfile)
-	for row in reader:		# iterates rows from input CSV file in order
+	## iterate rows in order
+	for row in reader:
+		# the if statements check whether working on first row; if first row,
+		# it gets treated as a header and built appropriately; other rows are
+		# built as "records"
 		if rownum == 0:
+			# when row number is 0, this row is built as the header
 			header = row
 			partidx = []
 			vmpidx = []
@@ -75,26 +96,40 @@ try:
 			# Line-specific parameters
 			for linenum in range(1, 50):
 				try:
-					partidx.append(header.index('Route Partition ' + str(linenum)))
-					vmpidx.append(header.index('Voice Mail Profile ' + str(linenum)))
-					linecssidx.append(header.index('Line CSS ' + str(linenum)))
-					fwdallcssidx.append(header.index('Forward All CSS ' + str(linenum)))
-					fwdcssidx.append(header.index('Forward Busy Internal CSS ' + str(linenum)))
-					fwdcssidx.append(header.index('Forward Busy External CSS ' + str(linenum)))
-					fwdcssidx.append(header.index('Forward No Answer Internal CSS ' + str(linenum)))
-					fwdcssidx.append(header.index('Forward No Answer External CSS ' + str(linenum)))
-					fwdcssidx.append(header.index('Forward No Coverage Internal CSS ' + str(linenum)))
-					fwdcssidx.append(header.index('Forward No Coverage External CSS ' + str(linenum)))
-					fwdcssidx.append(header.index('Forward on CTI Failure CSS ' + str(linenum)))
-					fwdcssidx.append(header.index('Forward Unregistered Internal CSS ' + str(linenum)))
-					fwdcssidx.append(header.index('Forward Unregistered External CSS ' + str(linenum)))
+					partidx.append(header.index(
+						'Route Partition ' + str(linenum)))
+					vmpidx.append(header.index(
+						'Voice Mail Profile ' + str(linenum)))
+					linecssidx.append(header.index(
+						'Line CSS ' + str(linenum)))
+					fwdallcssidx.append(header.index(
+						'Forward All CSS ' + str(linenum)))
+					fwdcssidx.append(header.index(
+						'Forward Busy Internal CSS ' + str(linenum)))
+					fwdcssidx.append(header.index(
+						'Forward Busy External CSS ' + str(linenum)))
+					fwdcssidx.append(header.index(
+						'Forward No Answer Internal CSS ' + str(linenum)))
+					fwdcssidx.append(header.index(
+						'Forward No Answer External CSS ' + str(linenum)))
+					fwdcssidx.append(header.index(
+						'Forward No Coverage Internal CSS ' + str(linenum)))
+					fwdcssidx.append(header.index(
+						'Forward No Coverage External CSS ' + str(linenum)))
+					fwdcssidx.append(header.index(
+						'Forward on CTI Failure CSS ' + str(linenum)))
+					fwdcssidx.append(header.index(
+						'Forward Unregistered Internal CSS ' + str(linenum)))
+					fwdcssidx.append(header.index(
+						'Forward Unregistered External CSS ' + str(linenum)))
 				except ValueError:
 					break
 			## BLF-specific parameters
 			blfidx = []
 			for blfnum in range (1, 199):
 				try:
-					blfidx.append(header.index('Busy Lamp Field Directory Number ' + str(blfnum)))
+					blfidx.append(header.index(
+						'Busy Lamp Field Directory Number ' + str(blfnum)))
 				except ValueError:
 					break
 			## remove the following fields which will not work in 10.5
@@ -110,8 +145,10 @@ try:
 					header.remove('User ID ' + str(userid))
 				except ValueError:
 					break
-			a.writerows([header])			
+			a.writerows([header])
 		else:
+			# for all other rows, process each column as per indexes created
+			# in header step
 			colnum = 0
 			phone = []
 			for col in row:
@@ -146,6 +183,7 @@ try:
 				colnum += 1
 			a.writerows([phone])
 		rownum += 1
+# end of script
 finally:
-	f.close()
-	outputfile.close()
+	f.close()	# close the input file
+	outputfile.close()	# close the output file
